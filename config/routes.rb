2305a8c100c200017cb260553f5a00db57666ddc
Thomas Wendling <axentium@yahoo.com>
2013-11-14 13:39:15 -0700

    try

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Treebook::Application.routes.draw do
   # root 'welcome#index'
 
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+    get 'statuses' => 'statuses#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
