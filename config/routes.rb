52872a6cb80ed127b907f90a79a8f502160311f9
Thomas Wendling <axentium@yahoo.com>
2013-11-14 13:43:59 -0700

    try

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Treebook::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   root 'statuses#index'
 
   # Example of regular route:
     get 'statuses' => 'statuses#index'
