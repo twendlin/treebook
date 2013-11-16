87b63a1594624ed3afb23c623d5c68bdaadb43d1
Thomas Wendling <axentium@yahoo.com>
2013-11-16 12:53:48 -0700

    try

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Treebook::Application.routes.draw do
   
 
   # The priority is based upon order of creation: first created -> highest priority.
-  # See how aall your routes lay out with "rake routes".
+  # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
    root 'statuses#index'
