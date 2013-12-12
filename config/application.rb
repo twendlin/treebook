c45428b72314e8232b131eb06c8cd0b8688588d1
Tom Wendling <axentium@yahoo.com>
2013-12-12 09:37:11 -0700

    try

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,14 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
+
+
+
+config.assets.initialize_on_precompile = false
+
+
+
+
 module Treebook
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
